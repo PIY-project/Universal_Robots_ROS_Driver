8c1867117fe79b6dfa3405bcc2db1fd8e37244a3
Matteo-Exsensia <matteo.milani@exsensia.ai>
2026-05-06 15:10:04 +0200

    feat(free_jog): change executiuon policy
    Removed reorient mode, not needed anymore.
    Removed free jog active requirement, added freejog restart if params are
    changed while freejog is active.

--- ur_robot_driver/src/rpwc_ur_bridge_native.cpp
+++ ur_robot_driver/src/rpwc_ur_bridge_native.cpp
@@ -303,12 +303,6 @@ bool callback_robot_curr_pose(rpwc_msgs::robotArmState::Request& req, rpwc_msgs:
 
 bool callback_set_free_jog_params(rpwc_msgs::setFreeJogParams::Request& req, rpwc_msgs::setFreeJogParams::Response& res)
 {
-  if(!freedrive_)
-  {
-    res.success.data = false;
-    res.info.data = "Robot must be in Free Jog mode to set params";
-  }
-
   switch (req.mode.data)
   {
   case 0:   // Joints
@@ -331,16 +325,6 @@ bool callback_set_free_jog_params(rpwc_msgs::setFreeJogParams::Request& req, rpw
     freedrive_params_.ref_frame = urcl::control::FreedriveReferenceFrame(req.ref_frame.data);
     break;
 
-  case 2:   // Reorient
-    freedrive_params_.lock_x = true;
-    freedrive_params_.lock_y = true;
-    freedrive_params_.lock_z = true;
-    freedrive_params_.lock_rx = req.lock_rx.data;
-    freedrive_params_.lock_ry = req.lock_ry.data;
-    freedrive_params_.lock_rz = req.lock_rz.data;
-    freedrive_params_.ref_frame = urcl::control::FreedriveReferenceFrame::BASE;
-    break;
-
   default:
     ROS_ERROR_STREAM("Unknow Free Jog mode: " << req.mode.data);
     res.success.data = false;
@@ -348,8 +332,27 @@ bool callback_set_free_jog_params(rpwc_msgs::setFreeJogParams::Request& req, rpw
   }
 
   freedrive_params_.mode = urcl::control::FreedriveMode(req.mode.data);
-  res.success.data = true;
 
+
+  if (freedrive_)
+  {
+    std::lock_guard<std::mutex> lock(send_command_mutex_);
+    if (!ur_driver_->writeFreedriveControlMessage(urcl::control::FreedriveControlMessage::FREEDRIVE_STOP))
+    {
+      res.success.data = false;
+      res.info.data = "Failed to disable Free Jog";
+      return true;
+    }
+
+    if (!ur_driver_->writeFreedriveControlMessage(urcl::control::FreedriveControlMessage::FREEDRIVE_START, freedrive_params_, urcl::RobotReceiveTimeout::millisec(200)))
+    {
+      res.success.data = false;
+      res.info.data = "Failed to enable Free Jog";
+      return true;
+    }
+  }
+
+  res.success.data = true;
   return true;
 }
 
